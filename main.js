2de0609babad03184bd26bd2145324aa76f8fc34
techwiz865 <147875298+techwiz865@users.noreply.github.com>
2024-01-19 13:37:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+splat.onclick = readySetStart;
+
+
 function readySetStart() {
-  
+  const newElement = document.createElement("p");
+  const newContent = document.createTextNode("OK, then let's start!");
+  newElement.appendChild(newContent);
+  const currentPlace = document.getElementById("div1");
+  document.body.insertBefore(newElement, currentPlace);
+
 }
